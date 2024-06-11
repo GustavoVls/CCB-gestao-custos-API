03eaff20d5f4ae80a330925987308485f121829e
GustavoVls <gustavoveloso.almeida@gmail.com>
2024-06-10 22:31:39 -0400

    Adicionando campos na model de Prioridades

--- src/main/java/com/ccbgestaocustosapi/models/Prioridades.java
+++ src/main/java/com/ccbgestaocustosapi/models/Prioridades.java
@@ -1,4 +1,27 @@
 package com.ccbgestaocustosapi.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PRIORIDADES",schema="CCB",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"prioridade_nome"})
+        })
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Prioridades {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "prioridades_prioridade_id_seq")
+    @SequenceGenerator(schema = "CCB", name = "prioridades_prioridade_id_seq",
+            sequenceName = "prioridades_prioridade_id_seq", allocationSize = 1)
+    @Column(name = "prioridade_id", nullable = false)
+    private Integer idCategoria;
+
+    @Column(name = "prioridade_nome", nullable = false)
+    private String nomePrioridade;
+
 }
